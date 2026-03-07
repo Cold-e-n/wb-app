486ead3a7721a5b8359db0d4adfa87025eb61160
Cold-e-n <cold.e81369@gmail.com>
2026-03-07 23:18:07 +0700

    Update UI 'color-layout'.

--- src/routes/_auth/color-layouts/add-new.tsx
+++ src/routes/_auth/color-layouts/add-new.tsx
@@ -1,29 +1,46 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { Link } from '@tanstack/react-router'
 import { Button } from '@/components/ui/button'
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from '@/components/ui/tooltip'
+
 import { ColorLayoutForm } from '@/features/color-layout/components/color-layout-form'
 
-import { ArrowLeft } from 'lucide-react'
+import { MoveLeft } from 'lucide-react'
 
 const RouteComponent = () => {
   return (
     <div className="flex flex-1 flex-col gap-4 sm:gap-6">
-      <div className="flex flex-wrap items-center justify-between gap-2">
-        <div className="space-y-3">
-          <h1 className="text-2xl font-bold tracking-tight">
-            Tambah Layout Benang Warna
-          </h1>
+      <div className="flex flex-wrap items-center justify-between gap-2 no-print">
+        <div className="space-y-1">
+          <div className="flex items-center gap-5 mb-1">
+            <div className="no-print">
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <Button
+                    variant="outline"
+                    size="icon"
+                    asChild
+                    className="-ml-2"
+                  >
+                    <Link to="/color-layouts">
+                      <MoveLeft className="h-4 w-4" />
+                    </Link>
+                  </Button>
+                </TooltipTrigger>
+                <TooltipContent>Kembali</TooltipContent>
+              </Tooltip>
+            </div>
+            <h1 className="text-2xl font-bold tracking-tight">
+              Tambah Layout Benang Warna
+            </h1>
+          </div>
         </div>
       </div>
 
-      <div className="flex flex-wrap">
-        <Button variant="outline" asChild>
-          <Link to="/color-layouts">
-            <ArrowLeft className="mr-2 h-4 w-4" />
-            Kembali
-          </Link>
-        </Button>
-      </div>
       <ColorLayoutForm />
     </div>
   )
