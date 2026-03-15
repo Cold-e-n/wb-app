047d5b2be89e34f89ddbe27ccd6ee0f84add9a6e
Cold-e-n <cold.e81369@gmail.com>
2026-03-15 19:02:33 +0700

    Update UI 'dashboard'.

--- src/features/dashboard/components/dashboard-card.tsx
+++ src/features/dashboard/components/dashboard-card.tsx
@@ -1,4 +1,3 @@
-import { Link } from '@tanstack/react-router'
 import { useDashboardStats } from '../hooks/use-dashboard'
 import {
   Card,
@@ -18,7 +17,6 @@ type StatCard = {
   title: string
   value: number
   icon: React.ElementType
-  href: string
 }
 
 export const DashboardCard = () => {
@@ -29,46 +27,40 @@ export const DashboardCard = () => {
       title: 'Total Kain',
       value: data.totalFabrics,
       icon: Scroll,
-      href: '/fabrics',
     },
     {
       title: 'Benang Warna',
       value: data.totalColors,
       icon: Palette,
-      href: '/color',
     },
     {
       title: 'Layout Warna',
       value: data.totalColorLayouts,
       icon: AlignHorizontalJustifyCenter,
-      href: '/color-layouts',
     },
     {
       title: 'Posisi Warna',
       value: data.totalColorPositions,
       icon: Blocks,
-      href: '/color-positions',
     },
   ]
 
   return (
     <div className="grid gap-4 lg:gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
       {cards.map((card) => (
-        <Link key={card.href} to={card.href} className="block">
-          <Card className="hover:shadow-md transition-shadow cursor-pointer h-full gap-3">
-            <CardHeader>
-              <CardTitle className="text-sm font-medium text-muted-foreground">
-                {card.title}
-              </CardTitle>
-              <CardAction>
-                <card.icon className="h-4 w-4 text-muted-foreground" />
-              </CardAction>
-            </CardHeader>
-            <CardContent>
-              <div className="text-2xl font-bold">{card.value}</div>
-            </CardContent>
-          </Card>
-        </Link>
+        <Card className="hover:shadow-md transition-shadow h-full gap-3">
+          <CardHeader>
+            <CardTitle className="text-sm font-medium text-muted-foreground">
+              {card.title}
+            </CardTitle>
+            <CardAction>
+              <card.icon className="h-4 w-4 text-muted-foreground" />
+            </CardAction>
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-bold">{card.value}</div>
+          </CardContent>
+        </Card>
       ))}
     </div>
   )
