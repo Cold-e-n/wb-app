eeb918db582279b47d9da5b941d2cd6d2ea9c551
Cold-e-n <cold.e81369@gmail.com>
2026-05-07 10:11:42 +0700

    Hapus 'type' yang enggak dipakai.

--- src/routes/_auth/color-positions/index.tsx
+++ src/routes/_auth/color-positions/index.tsx
@@ -1,6 +1,5 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { useSuspenseQuery } from '@tanstack/react-query'
-import type {ColorPositionWithRelations} from '@/types/ColorPosition';
 import {
   ColorPositions,
   ColorPositionsProvider,
